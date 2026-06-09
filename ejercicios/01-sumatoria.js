a3e6f2c554bfd53f465d90f0a7d4a423e537e67c
GabrielRoda <jawedliop@gmail.com>
2026-06-09 10:51:14 -0300

    resolvimos el ejercicio de sumatoria

---
+++ ejercicios/01-sumatoria.js
@@ -0,0 +1,13 @@
+//suma de dos numeros y muestra el resultado
+
+let numero1, numero2, resultado
+
+numero1 = parseInt(prompt("Ingresa el primer numero"))
+console.log(typeof(numero1))
+console.log(numero1)
+
+numero2 = parseInt(prompt("Ingresa el segundo numero"))
+console.log(numero2)
+
+resultado = numero1 + numero2
+console.log("La suma de los dos numeros es = " + resultado)
---
+++ js/01-variables.js
@@ -0,0 +1,21 @@
+//comentario de una linea
+
+/*Comentario de múltiples líneas*/
+
+//crear una variable (let, const, var)
+
+const numero1 = 5
+let nombre = "Candelaria"
+console. log("Variable nombre: " + nombre)
+
+//modificar el nombre
+nombre = "Garzia"
+
+//mostrar en consola
+console. log("Variable nombre: " + nombre)
+
+//tipos de datos: valores primitivos, valores por referencia
+const numeroEntero = 10
+const numeroDecimal = 10.50
+console. log(typeof(numeroEntero), "numeroEntero=" + numeroEntero)
+console. log(typeof(numeroDecimal), "numeroDecimal=" + numeroDecimal)
