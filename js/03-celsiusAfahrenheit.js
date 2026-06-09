925552cc5395a376f79fba95461e45bd7ced4732
GabrielRoda <jawedliop@gmail.com>
2026-06-09 20:49:20 -0300

    Se realizó el ejercicio de celsius a fahrenheit

---
+++ js/03-celsiusAfahrenheit.js
@@ -0,0 +1,7 @@
+let celsius, fahren
+console.log(typeof(celsius))
+console.log(celsius)
+celsius = parseInt(prompt("Ingresa los grados Celsius"))
+
+fahren = celsius * 1.8 + 32
+console.log("En grados Fahrenheit serían: " + fahren)
