0ff79a9f7c6dec22a2394c7c83f0af8f64d75adf
GabrielRoda <jawedliop@gmail.com>
2026-06-09 20:44:57 -0300

    Se realizó el ejercicio de hora/s a minutos

---
+++ js/02-horaMinuto.js
@@ -0,0 +1,8 @@
+let horas, resultado
+console.log(typeof(horas))
+console.log(horas)
+horas = parseInt(prompt("Ingresa la cantidad de hora/s"))
+
+resultado = horas * 60
+console.log("La cantidad de minutos serían: " + resultado)
+
